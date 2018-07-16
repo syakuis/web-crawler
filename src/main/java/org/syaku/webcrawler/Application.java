1c6197117164653a9f8db13fc4ea46fd38766f8d
최석균 <syaku@naver.com>
2018-07-16 23:45:51 +0900

    start

---
+++ src/main/java/org/syaku/webcrawler/Application.java
@@ -0,0 +1,15 @@
+package org.syaku.webcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @since 2018. 7. 13.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
